eefbefa693d36b69143c5911cba97a530b08b3f1
Ahmed qotb <dev.ahmedqotb@gmail.com>
2025-02-03 20:17:05 +0200

    update

--- api/controllers/LookupsController.php
+++ api/controllers/LookupsController.php
@@ -2,14 +2,11 @@
 
 namespace api\controllers;
 
-use api\resources\FaqResource;
 use api\resources\SponsorResource;
-use common\helpers\Helper;
 use Yii;
 use api\helpers\ResponseHelper;
-use api\resources\CountryResource;
 
-class LookupsControllerMy extends MyRestUnAuthController
+class LookupsController extends MyRestUnAuthController
 {
 
 
