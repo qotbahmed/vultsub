cbb90dffb8bc9bc10571833fce997c7f22f3ed56
Ahmed qotb <dev.ahmedqotb@gmail.com>
2025-02-06 16:07:37 +0200

    update

---
+++ api/controllers/PointsLogsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace api\controllers;
+
+use common\models\PointsLogs;
+use common\models\User;
+use common\models\UserProfile;
+use Yii;
+use api\models\UserForm;
+use api\helpers\ImageHelper;
+use api\models\ChangePassword;
+use api\helpers\ResponseHelper;
+use api\resources\PointsLogsResource;
+use yii\base\InvalidParamException;
+
+class PointsLogsController extends MyActiveController
+{
+
+    public $modelClass = PointsLogsResource::class;
+
+    public function actionIndex()
+    {
+        $pointsLogs = PointsLogsResource::find()->where(\Yii::$app->user->identity->id)->all();
+        return ResponseHelper::sendSuccessResponse($pointsLogs);
+    }
+
+
+
+
+
+}
