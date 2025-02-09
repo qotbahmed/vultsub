2cebe0feabd22fe1f43063cd0d32d2cdbace01e4
Ahmed qotb <dev.ahmedqotb@gmail.com>
2025-02-09 19:03:22 +0200

    update

--- api/controllers/ProfileController.php
+++ api/controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace api\controllers;
 
+use backend\models\Settings;
 use common\models\PointsLogs;
 use common\models\User;
 use common\models\UserProfile;
@@ -56,25 +57,57 @@ class ProfileController extends MyActiveController
     }
     public function actionCheckPoint()
     {
-        $user = UserProfile::findOne(['user_id'=>\Yii::$app->user->identity->id]);
         $params = \Yii::$app->request->post();
+        $user_id = Yii::$app->user->identity->id;
+        $total_time = $params['total_time'] ?? 0;
+        $page_count = $params['page_count'] ?? 0; // Number of pages read
 
+        $user = UserProfile::findOne(['user_id' => $user_id]);
 
         if (isset($params['surah'], $params['ayah_num'], $params['page_num'])) {
-            $user->surah = $params['surah']??'';
-            $user->ayah_num = $params['ayah_num']??0;
-            $user->points_num += $params['points_num']??0;
-            $user->page_num = $params['page_num']??0;
+
+            $settings = Settings::find()->select([
+                'points_per_second',
+                'daily_points',
+                'reading_points_delay',
+                'max_daily_points_per_user'
+            ])->one();
+
+            $todayPoints = PointsLogs::find()
+                ->where(['user_id' => $user_id])
+                ->andWhere(['>=', 'created_at', date('Y-m-d 00:00:00')])
+                ->sum('points_num') ?? 0;
+
+            $expected_pages_read = floor($total_time / $settings->reading_points_delay);
+            $valid_pages = min($expected_pages_read, $page_count);
+
+            if ($valid_pages < 1) {
+                return ResponseHelper::sendFailedResponse(['error' => Yii::t('common', 'Reading time is too short to earn points.')]);
+            }
+
+            $earned_time = $valid_pages * $settings->reading_points_delay; // Time that is valid
+            $earned_points = $earned_time * $settings->points_per_second;
+
+            $new_total_points = $todayPoints + $earned_points;
+            if ($new_total_points > $settings->max_daily_points_per_user) {
+                $earned_points = max(0, $settings->max_daily_points_per_user - $todayPoints);
+            }
+
+            $user->surah = $params['surah'] ?? '';
+            $user->ayah_num = $params['ayah_num'] ?? 0;
+            $user->points_num += $earned_points ?? 0;
+            $user->page_num = $params['page_num'] ?? 0;
 
             if ($user->save()) {
+                // **Log the earned points**
                 $log = new PointsLogs();
                 $log->user_id = $user->user_id;
                 $log->user_name = $user->firstname;
                 $log->user_mobile = $user->user->mobile;
-                $log->points_num = $params['points_num']??0;
+                $log->points_num = $earned_points;
                 $log->type = PointsLogs::TYPE_ADD;
                 $log->page_num = $params['page_count']??0;
-                $log->time =$params['time'];
+                $log->time = $earned_time; // Log valid time
 
                 if (!$log->save()) {
                     return ResponseHelper::sendFailedResponse($log->getFirstErrors());
@@ -88,7 +121,49 @@ class ProfileController extends MyActiveController
         }
     }
 
+    public function actionCalculatePoints()
+    {
+        $params = Yii::$app->request->post();
+        $user_id = Yii::$app->user->identity->id;
+        $total_time = $params['total_time'] ?? 0;
+        $page_numbers = $params['page_numbers'] ?? 0;
 
+        $settings = Settings::find()->select([
+            'points_per_second',
+            'daily_points',
+            'reading_points_delay',
+            'max_daily_points_per_user'
+        ])->one();
+
+        if (!$settings) {
+            return ResponseHelper::sendFailedResponse(['error' => Yii::t('common', 'Settings not found.')]);
+        }
+
+        $todayPoints = PointsLogs::find()
+            ->where(['user_id' => $user_id])
+            ->andWhere(['>=', 'created_at', date('Y-m-d 00:00:00')])
+            ->sum('points_num') ?? 0;
+
+        $expected_time = $params['page_count'] * $settings->reading_points_delay;
+
+        if ($total_time < $expected_time) {
+            return ResponseHelper::sendFailedResponse([
+                'error' => Yii::t('common', 'Reading time is too short for the pages read.')
+            ]);
+        }
+
+        $earned_points = $total_time * $settings->points_per_second;
+        $new_total_points = $todayPoints + $earned_points;
+
+        if ($new_total_points > $settings->max_daily_points_per_user) {
+            $earned_points = max(0, $settings->max_daily_points_per_user - $todayPoints);
+        }
+
+        return ResponseHelper::sendSuccessResponse([
+            'earned_points' => $earned_points,
+            'total_today' => $new_total_points
+        ]);
+    }
     public function actionChangePassword()
     {
         $model = new ChangePassword();
