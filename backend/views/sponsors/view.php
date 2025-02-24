10f82730ba4c892b85b1acefd836a96ce4e2ee62
Ahmed qotb <dev.ahmedqotb@gmail.com>
2025-02-24 21:10:59 +0200

    update

--- backend/views/sponsors/view.php
+++ backend/views/sponsors/view.php
@@ -17,7 +17,7 @@ $totalDonations = array_sum(array_column($model->logs, 'amount'));
 ?>
 <div class="sponsor-info border-bottom pb-3 mb-4">
     <h4 class="text-primary m-0">
-        <?= Yii::t('backend', 'شركة الراعي') ?>: <?= Html::encode($this->title) ?>
+         <?= Html::encode($model->title) ?>
     </h4>
     <div class="text-muted small mt-2">
         <span class="mx-2">•</span>
